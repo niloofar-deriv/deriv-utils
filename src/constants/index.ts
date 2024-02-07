ea2d47111186db08c565dbab7967b65d763f5e74
yashim-deriv <yashim@regentmarkets.com>
2024-02-07 15:47:54 +0800

    feat: add currency constants (#3)

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,5 @@
 import * as AppIDConstants from "./app-id.constants";
+import * as CurrencyConstants from "./currency.constants";
 import * as LocalStorageConstants from "./localstorage.constants";
 
-export { AppIDConstants, LocalStorageConstants };
+export { AppIDConstants, CurrencyConstants, LocalStorageConstants };
