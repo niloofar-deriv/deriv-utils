02b38096d510dea7290671b461855419179321e0
Niloofar <niloofar.sadeghi@regentmarkets.com>
2024-02-23 11:37:31 +0800

    feat: url constants

---
+++ src/constants/url.constants.ts
@@ -0,0 +1,20 @@
+export const deriv = 'deriv.com';
+export const derivMe = 'deriv.me';
+export const derivBe = 'deriv.be';
+
+export const supportedDomains = [deriv, derivBe, derivMe]
+
+export const binaryBotProduction = `https://bot.${domainUrl}`
+
+
+
+// BINARYBOT_PRODUCTION: ,
+//     BINARYBOT_STAGING: `https://staging-bot.${domainUrl}`,
+//     DERIV_APP_PRODUCTION: `https://app.${domainUrl}`,
+//     DERIV_APP_STAGING: `https://staging-app.${domainUrl}`,
+//     DERIV_COM_PRODUCTION: `https://${domainUrl}`,
+//     DERIV_COM_PRODUCTION_EU: `https://eu.${domainUrl}`,
+//     DERIV_COM_STAGING: `https://staging.${domainUrl}`,
+//     DERIV_HOST_NAME: domainUrl,
+//     SMARTTRADER_PRODUCTION: `https://smarttrader.${domainUrl}`,
+//     SMARTTRADER_STAGING: `https://staging-smarttrader.${domainUrl}`,
