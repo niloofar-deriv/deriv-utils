4375a195d5860e04958f7bbc4e1cf31dde482afa
Niloofar <niloofar.sadeghi@regentmarkets.com>
2024-02-26 15:42:56 +0800

    feat: url constants

--- src/constants/url.constants.ts
+++ src/constants/url.constants.ts
@@ -1,20 +1,30 @@
-export const deriv = 'deriv.com';
-export const derivMe = 'deriv.me';
-export const derivBe = 'deriv.be';
+export const deriv = "deriv.com";
+export const derivMe = "deriv.me";
+export const derivBe = "deriv.be";
 
-export const supportedDomains = [deriv, derivBe, derivMe]
+export const supportedDomains = [deriv, derivBe, derivMe] as const;
+export const baseDomain = (typeof window !== "undefined" &&
+    window.location.hostname.split("app.")[1]) as (typeof supportedDomains)[number];
+export const domain = supportedDomains.includes(baseDomain) ? baseDomain : deriv;
 
-export const binaryBotProduction = `https://bot.${domainUrl}`
+// deriv URLs
+export const binaryBotProduction = `https://bot.${domain}` as const;
+export const binaryBotStaging = `https://staging-bot.${domain}` as const;
+export const derivAppProduction = `https://app.${domain}` as const;
+export const derivAppStaging = `https://staging-app.${domain}` as const;
+export const derivComProduction = `https://${domain}` as const;
+export const derivComProductionEU = `https://eu.${domain}` as const;
+export const derivComStaging = `https://staging.${domain}` as const;
+export const derivHost = domain;
+export const smartTraderProduction = `https://smarttrader.${domain}` as const;
+export const smartTraderStaging = `https://staging-smarttrader.${domain}` as const;
+//
 
+export const whatsApp = "https://wa.me/35699578341";
 
+export const queryParameters = {
+    lang: "lang",
+    action: "action",
+} as const;
 
-// BINARYBOT_PRODUCTION: ,
-//     BINARYBOT_STAGING: `https://staging-bot.${domainUrl}`,
-//     DERIV_APP_PRODUCTION: `https://app.${domainUrl}`,
-//     DERIV_APP_STAGING: `https://staging-app.${domainUrl}`,
-//     DERIV_COM_PRODUCTION: `https://${domainUrl}`,
-//     DERIV_COM_PRODUCTION_EU: `https://eu.${domainUrl}`,
-//     DERIV_COM_STAGING: `https://staging.${domainUrl}`,
-//     DERIV_HOST_NAME: domainUrl,
-//     SMARTTRADER_PRODUCTION: `https://smarttrader.${domainUrl}`,
-//     SMARTTRADER_STAGING: `https://staging-smarttrader.${domainUrl}`,
+export type queryParameters = keyof typeof queryParameters;
