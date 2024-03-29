4eed896d87fadde221cbc800644ce447a849b7ad
heorhi-deriv <heorhi@binary.com>
2024-03-29 17:36:13 +0300

    feat: :sparkles: add file utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,9 @@
 import * as FormatUtils from "./format.utils";
+import * as FileUtils from "./file.utils";
 import * as LocalStorageUtils from "./localstorage.utils";
 import * as ObjectUtils from "./object.utils";
 import * as PromiseUtils from "./promise.utils";
 import * as URLUtils from "./url.utils";
 import * as WebSocketUtils from "./websocket.utils";
 
-export { FormatUtils, LocalStorageUtils, ObjectUtils, PromiseUtils, URLUtils, WebSocketUtils };
+export { FileUtils, FormatUtils, LocalStorageUtils, ObjectUtils, PromiseUtils, URLUtils, WebSocketUtils };
