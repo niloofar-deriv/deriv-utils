16835a93c4aef4f4cb57fb58d40037d1767cd278
Niloofar <niloofar.sadeghi@regentmarkets.com>
2024-04-15 17:37:47 +0800

    build: define domain configs

--- utils-docs/docusaurus.config.ts
+++ utils-docs/docusaurus.config.ts
@@ -8,15 +8,15 @@ const config: Config = {
     favicon: "img/logo.svg",
 
     // Set the production url of your site here
-    url: "https://deriv-utility-library.com",
+    url: "https://deriv-com.github.io",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: "/",
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
-    organizationName: "Deriv", // Usually your GitHub org/user name.
-    projectName: "@deriv-com/utils", // Usually your repo name.
+    organizationName: "deriv-com", // Usually your GitHub org/user name.
+    projectName: "deriv-utils", // Usually your repo name.
 
     onBrokenLinks: "throw",
     onBrokenMarkdownLinks: "warn",
