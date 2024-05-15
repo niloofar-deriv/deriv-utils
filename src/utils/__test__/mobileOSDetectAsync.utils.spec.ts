88d871a0227b869910cc229917ceaedc2f4d9b7e
shontzu-deriv <shon.tzu@regentmarkets.com>
2024-05-15 17:43:52 +0800

    fix: jest -> vitest

--- src/utils/__test__/mobileOSDetectAsync.utils.spec.ts
+++ src/utils/__test__/mobileOSDetectAsync.utils.spec.ts
@@ -1,44 +1,36 @@
-import { mobileOSDetectAsync, getOSNameWithUAParser } from "../mobileOSDetectAsync.utils";
-import UAParser from "ua-parser-js";
+import { describe, expect, test } from "vitest";
+import { mobileOSDetectAsync } from "../mobileOSDetectAsync.utils";
 
 describe("mobileOSDetectAsync", () => {
     test('should return "Windows Phone" for Windows Phone user agent', async () => {
-        jest.spyOn(window.navigator, "userAgent", "get").mockReturnValue("windows phone");
+        Object.defineProperty(window.navigator, "userAgent", {
+            value: "windows phone",
+            configurable: true,
+        });
         expect(await mobileOSDetectAsync()).toBe("Windows Phone");
     });
 
     test('should return "Android" for Android user agent', async () => {
-        jest.spyOn(window.navigator, "userAgent", "get").mockReturnValue("android");
+        Object.defineProperty(window.navigator, "userAgent", {
+            value: "android",
+            configurable: true,
+        });
         expect(await mobileOSDetectAsync()).toBe("Android");
     });
 
     test('should return "iOS" for iOS user agent', async () => {
-        jest.spyOn(window.navigator, "userAgent", "get").mockReturnValue("iPhone");
-        jest.spyOn(window, "MSStream", "get").mockReturnValue({
-            msClose: () => {},
-            msDetachStream: () => {},
-            type: "someType",
-        });
-        expect(await mobileOSDetectAsync()).toBe("iOS");
-    });
-
-    test('should return "iOS" for iOS user agent', async () => {
-        jest.spyOn(window.navigator, "userAgent", "get").mockReturnValue("iPhone");
-        jest.spyOn(window, "MSStream", "get").mockReturnValue({
-            msClose: () => {},
-            msDetachStream: () => {},
-            type: "someType",
+        Object.defineProperty(window.navigator, "userAgent", {
+            value: "iPhone",
+            configurable: true,
         });
         expect(await mobileOSDetectAsync()).toBe("iOS");
     });
 
     test('should return "unknown" for unknown user agent', async () => {
-        jest.spyOn(window.navigator, "userAgent", "get").mockReturnValue("unknown");
+        Object.defineProperty(window.navigator, "userAgent", {
+            value: "unknown",
+            configurable: true,
+        });
         expect(await mobileOSDetectAsync()).toBe("unknown");
-    });
-
-    test("should return the OS name", () => {
-        jest.spyOn(UAParser, "OS", "get").mockReturnValue({ NAME: "name", VERSION: "version" });
-        expect(getOSNameWithUAParser()).toBe("name");
     });
 });
