cb7e4ea6c65915314aa789803863baab43388064
yashim-deriv <yashim@regentmarkets.com>
2024-07-26 16:15:42 +0800

    feat: add verify email storage key

--- src/constants/localstorage.constants.ts
+++ src/constants/localstorage.constants.ts
@@ -1,5 +1,6 @@
 export const activeLoginid = "client.active_loginid";
 export const accountList = "client.account_list";
+export const lastVerifyEmail = "client.verify_email";
 export const configAppId = "config.app_id";
 export const configServerURL = "config.server_url";
 export const i18nLanguage = "i18n_language";
