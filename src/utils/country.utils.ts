3af8a83b57631bdc350d2740bc1200a9feb88182
Prince <prince@deriv.com>
2024-11-06 13:48:29 +0400

    chore: exported the component and added jsdocs

--- src/utils/country.utils.ts
+++ src/utils/country.utils.ts
@@ -1,9 +1,23 @@
 import Cookies from "js-cookie";
 import { cloudflareTrace } from "../constants/url.constants";
 
-interface TraceData {
+type TraceData = {
     loc?: string;
-}
+};
+
+/**
+ * Fetches the country information based on Cloudflare's trace data or a fallback from cookies.
+ * This function attempts to retrieve the country location by first fetching trace data from Cloudflare
+ * and then falling back to the location stored in the cookies if the fetch fails.
+ *
+ * @returns {Promise<string>} A Promise that resolves to a string representing the country code in lowercase.
+ *                            Returns an empty string if no country data is available or if an error occurs.
+ *
+ * @example
+ * // Returns the country code in lowercase based on Cloudflare's trace data or cookies.
+ * getCountry().then(country => console.log(country));
+ */
+
 export const getCountry = async (): Promise<string> => {
     try {
         const response = await fetch(cloudflareTrace).catch(() => null);
